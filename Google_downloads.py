086fd49dcccec9de19355b05aa15ffde679623f8
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-8.local>
2016-06-03 18:18:45 +0100

    exception handling for when there are no records for that day

--- Google_downloads.py
+++ Google_downloads.py
@@ -43,25 +43,28 @@ while start_date < end_date:
     else:
         for app in app_list:
             print "Downloading download information for " + app
+            try:
+                if len(check_output(["gsutil", "ls", "gs://pubsite_prod_rev_02524245599547527969/stats/installs/%s%s_overview.csv" % (app, start_date.strftime("%Y%m"))])) > 0:
+                    print "Downloading Google downloads report for %s" % start_date.strftime("%Y%m")
+                    call(["gsutil", "cp", "gs://pubsite_prod_rev_02524245599547527969/stats/installs/%s%s_overview.csv" % (app, start_date.strftime("%Y%m")), "google_downloads_%s_%s_2.csv" % (app, start_date.strftime("%Y%m"))])
 
-            if len(check_output(["gsutil", "ls", "gs://pubsite_prod_rev_02524245599547527969/stats/installs/%s%s_overview.csv" % (app, start_date.strftime("%Y%m"))])) > 0:
-                print "Downloading Google downloads report for %s" % start_date.strftime("%Y%m")
-                call(["gsutil", "cp", "gs://pubsite_prod_rev_02524245599547527969/stats/installs/%s%s_overview.csv" % (app, start_date.strftime("%Y%m")), "google_downloads_%s_%s_2.csv" % (app, start_date.strftime("%Y%m"))])
+                else:
+                    print "No " + app + ' for this date'
 
-            else:
-                print "No " + app + ' for this date'
+                with open("google_downloads_%s_%s_2.csv" % (app, start_date.strftime("%Y%m")), 'rb') as source_file:
+                    with open("google_downloads_%s_%s.csv" % (app, start_date.strftime("%Y%m")), 'w+b') as dest_file:
+                        contents = source_file.read()
+                        dest_file.write(contents.decode('utf-16').encode('utf-8'))
 
-            with open("google_downloads_%s_%s_2.csv" % (app, start_date.strftime("%Y%m")), 'rb') as source_file:
-                with open("google_downloads_%s_%s.csv" % (app, start_date.strftime("%Y%m")), 'w+b') as dest_file:
-                    contents = source_file.read()
-                    dest_file.write(contents.decode('utf-16').encode('utf-8'))
+                print "Uploading Google sales report for %s" % start_date.strftime("%Y%m")
+                call(["s3cmd", "put", "google_downloads_%s_%s.csv" % (app, start_date.strftime("%Y%m")), "s3://bibusuu/Google_Downloads_Reports/%s/downloads_report_%s_%s.csv" % (start_date.strftime("%Y%m"), app, start_date.strftime("%Y%m"))])
 
-            print "Uploading Google sales report for %s" % start_date.strftime("%Y%m")
-            call(["s3cmd", "put", "google_downloads_%s_%s.csv" % (app, start_date.strftime("%Y%m")), "s3://bibusuu/Google_Downloads_Reports/%s/downloads_report_%s_%s.csv" % (start_date.strftime("%Y%m"), app, start_date.strftime("%Y%m"))])
+                print "Removing local file for %s" % start_date
+                os.remove("google_downloads_%s_%s.csv" % (app, start_date.strftime("%Y%m")))
+                os.remove("google_downloads_%s_%s_2.csv" % (app, start_date.strftime("%Y%m")))
 
-            print "Removing local file for %s" % start_date
-            os.remove("google_downloads_%s_%s.csv" % (app, start_date.strftime("%Y%m")))
-            os.remove("google_downloads_%s_%s_2.csv" % (app, start_date.strftime("%Y%m")))
+            except:
+                print "No app for this %s yet, maybe try tomorrow?" % start_date
 
     start_date = start_date + relativedelta(months=1)
 
